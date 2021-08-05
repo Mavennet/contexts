9558b98ff18dd16077e00dc98d44ce4b5a1ba61c
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2021-08-05 15:46:17 -0400

    attempt to fix context

--- mergeContexts.js
+++ mergeContexts.js
@@ -2,7 +2,14 @@ var fs = require("fs");
 
 const dir = "./v1.0";
 
-const newContext = {};
+const newContext = {
+  "@version": 1.1,
+  id: "@id",
+  type: "@type",
+  name: "https://schema.org/name",
+  description: "https://schema.org/description",
+  identifier: "https://schema.org/identifier",
+};
 
 function readFiles() {
   const filenames = fs.readdirSync(dir);
@@ -17,29 +24,3 @@ function readFiles() {
 }
 
 readFiles();
-
-// function changeFileNames() {
-//   const filenames = fs.readdirSync(dir);
-//   const result = filenames.filter((filename) => {
-//     return filename.split(".")[1] === "jsonld";
-//   });
-//   result.map((filename) => {
-//     // console.log(filename);
-//     const content = fs.readFileSync(dir + "/" + filename, "utf-8");
-//     if (filename.split("-.jsonld")[1] === "") {
-//       // console.log(filename);
-//       fs.rmSync(dir + "/" + filename);
-//     }
-//     // fs.rmSync(
-//     //   dir + "/" + filename.split(".jsonld.jsonld")[0] + ".jsonld.jsonld"
-//     // );
-//     // fs.writeFileSync(
-//     //   dir + "/" + filename.split("-.jsonld")[0] + ".jsonld",
-//     //   content
-//     // );
-//     // console.log(filename.split("-v1.0.jsonld")[0] + ".jsonld");
-//     // newContext[filename.split(".jsonld")[0]] = JSON.parse(content);
-//   });
-// }
-
-// changeFileNames();
